be74d55681b64e565bf9d86cee061ff3f1af546f
Myyyr <loic.themyr@gmail.com>
2021-06-29 12:02:56 +0200

    debug

--- cmain.py
+++ cmain.py
@@ -52,7 +52,7 @@ trainloader = torch.utils.data.DataLoader(
 
 # testset = torchvision.datasets.CIFAR10(
 #     root='./data', train=False, download=True, transform=transform_test)
-trainset = torchvision.datasets.Cityscapes('/local/DEEPLEARNING/cityscapes', split='test', mode='fine',
+testset = torchvision.datasets.Cityscapes('/local/DEEPLEARNING/cityscapes', split='test', mode='fine',
                      target_type='semantic')
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=2)
